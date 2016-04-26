dd7f7a46dd39e3b8b962fb6e5a7e68ee456f7934
Victor Tran <vicr12345@gmail.com>
2016-04-26 22:49:23 +1000

    Update Font

--- terminalwidget.cpp
+++ terminalwidget.cpp
@@ -10,7 +10,7 @@ terminalWidget::terminalWidget(QString workDir, QWidget *parent) : QTermWidget(0
     QFont font;
     font.setFamily("Noto Mono");
     font.setPointSize(10);
-    this->setFont(font);
+    this->setTerminalFont(font);
 
     QStringList environment;
     environment.append("TERM=xterm");
