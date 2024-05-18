d112c5fc66b4d2f601cb48e5c1056c80f5c01fde
Victor Tran <vicr12345@gmail.com>
2024-05-18 12:11:28 +1000

    Export init function

--- libtheterminal/theterminal-init.h
+++ libtheterminal/theterminal-init.h
@@ -1,8 +1,10 @@
 #ifndef THETERMINALINIT_H
 #define THETERMINALINIT_H
 
+#include "libtheterminal_global.h"
+
 namespace theTerminal {
-    void init();
+    void LIBTHETERMINAL_EXPORT init();
 }
 
 #endif // THETERMINALINIT_H
